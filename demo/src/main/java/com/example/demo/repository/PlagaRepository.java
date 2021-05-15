3e904238ee7cfa73abc41a0063fc415748f2ef58
Antonio Cuadrado <antoniocuad15@gmail.com>
2021-05-15 12:47:09 +0200

    Repositorios hechos

---
+++ demo/src/main/java/com/example/demo/repository/PlagaRepository.java
@@ -0,0 +1,8 @@
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.demo.model.Plaga;
+
+public interface PlagaRepository extends JpaRepository<Plaga,Long> {
+
+}
