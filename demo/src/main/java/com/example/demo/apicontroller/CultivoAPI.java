59d7dcc52c8bfafea0d24efde4c2b86c738a6892
Antonio Cuadrado <antoniocuad15@gmail.com>
2021-05-15 13:07:40 +0200

    API hecha con repositorios

--- demo/src/main/java/com/example/demo/apicontroller/CultivoAPI.java
+++ demo/src/main/java/com/example/demo/apicontroller/CultivoAPI.java
@@ -9,15 +9,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.controller.AppController;
 import com.example.demo.model.Cultivo;
+import com.example.demo.repository.CultivoRepository;
 
 @RestController
 public class CultivoAPI {
 
 	@Autowired
-	private AppController controlador;
+	private CultivoRepository repCultivos;
 	
 	@RequestMapping(value = "/getCultivos", method = RequestMethod.GET)
 	public List<Cultivo> getPeliculas() {
-		return controlador.cultivos();
+		return repCultivos.findAll();
 	}
 }
--- demo/src/main/java/com/example/demo/controller/AppController.java
+++ demo/src/main/java/com/example/demo/controller/AppController.java
@@ -4,23 +4,50 @@ import java.util.LinkedList;
 
 import javax.annotation.PostConstruct;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import com.example.demo.model.Cultivo;
 import com.example.demo.model.Especie;
+import com.example.demo.model.Plaga;
+import com.example.demo.model.Producto;
+import com.example.demo.model.Sustancia;
+import com.example.demo.repository.CultivoRepository;
+import com.example.demo.repository.EspecieRepository;
+import com.example.demo.repository.PlagaRepository;
+import com.example.demo.repository.ProductoRepository;
+import com.example.demo.repository.SustanciaRepository;
 
 @Controller
 public class AppController {
-	private LinkedList<Cultivo> lista = new LinkedList();
-	private LinkedList<Especie> lista2 = new LinkedList();
+	private LinkedList<Cultivo> listacultivos = new LinkedList<Cultivo>();
+	private LinkedList<Especie> listaespecies = new LinkedList<Especie>();
+	private LinkedList<Plaga> listaplagas = new LinkedList<Plaga>();
+	private LinkedList<Producto> listaproductos = new LinkedList<Producto>();
+	private LinkedList<Sustancia> listasustancias = new LinkedList<Sustancia>();
+	@Autowired
+	private CultivoRepository repCultivos;
+	@Autowired
+	private EspecieRepository repEspecies;
+	@Autowired
+	private PlagaRepository repPlagas;
+	@Autowired
+	private ProductoRepository repProductos;
+	@Autowired
+	private SustanciaRepository repSustancias;
 	
 	@PostConstruct
 	public void init() {
-		Cultivo c = new Cultivo("a",lista2);
-		lista.add(c);
+		Cultivo c = new Cultivo("a",listaespecies);
+		repCultivos.save(c);
+		Especie e = new Especie("nombreV","nombreC",listaplagas);
+		repEspecies.save(e);
+		Plaga p = new Plaga("nombreV","nombreC","url",listasustancias);
+		repPlagas.save(p);
+		Sustancia s = new Sustancia("nombre",listaproductos);
+		repSustancias.save(s);
+		Producto p2 = new Producto("nombre","url");
+		repProductos.save(p2);
 	}
-	
-	public LinkedList <Cultivo> cultivos(){
-		return lista;
-	}
+
 }
