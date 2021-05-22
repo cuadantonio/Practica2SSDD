e32f3647adebe102c3a6c6416b195a37add648bf
Antonio Cuadrado <antoniocuad15@gmail.com>
2021-05-22 17:19:39 +0200

    Estilos aplicados

--- demo/src/main/resources/static/js/script.js
+++ demo/src/main/resources/static/js/script.js
@@ -4,10 +4,10 @@ $(function() {
 	$.getJSON(urlPelicula,function(respuesta) {
 		
 		lista = $('#listaMain');
-		lista.append($('<ul/>').html("Cultivos:").attr("id","listaCultivos"));
+		lista.append($('<ul/>').html("Cultivos:").attr("id","listaCultivos").addClass('listaCultivos'));
 		for(cultivo in respuesta){
-			$('#listaCultivos').append($('<li/>').html("Nombre cultivo:"));
-			$('#listaCultivos').append($('<li>').html(respuesta[cultivo].nombre).attr("id",respuesta[cultivo].nombre));
+			$('#listaCultivos').append($('<li/>').html("Nombre cultivo:").addClass('cultivo'));
+			$('#listaCultivos').append($('<li>').html(respuesta[cultivo].nombre).attr("id",respuesta[cultivo].nombre).addClass('cultivo'));
 			$('#listaCultivos').append($('<button onClick="funcionEspecie(\''+respuesta[cultivo].nombre+'\')">').html("Especies").attr("id",respuesta[cultivo].nombre+'boton'));
 			$('#listaCultivos').append($('<button onClick="funcionEspecieBotones(\''+respuesta[cultivo].nombre+'\')">').html("Ocultar").attr("id",respuesta[cultivo].nombre+'boton2').hide());
 		}
@@ -22,25 +22,22 @@ $(function() {
 		$('#'+nombreCultivo+'boton').hide();
 		$('#'+nombreCultivo+'boton2').show();
 		lista2 = $('#'+nombreCultivo);
-		lista2.append($('<ul/>').html("Especies:").attr("id","listaEspecies"+nombreCultivo));
+		lista2.append($('<ul/>').html("Especies:").attr("id","listaEspecies"+nombreCultivo).addClass('listaEspecies'));
 		for(especie in respuesta){
-			$('#listaEspecies'+nombreCultivo).append($('<li/>').html("Nombre cientifico especie:").attr("id",respuesta[especie].nombreCientifico+'tag').hide());
-			$('#listaEspecies'+nombreCultivo).append($('<li>').html(respuesta[especie].nombreCientifico).attr("id",respuesta[especie].nombreCientifico).hide());
+			$('#listaEspecies'+nombreCultivo).append($('<li/>').html("Nombre cientifico especie:").attr("id",respuesta[especie].nombreCientifico+'tag').hide().addClass('especie'));
+			$('#listaEspecies'+nombreCultivo).append($('<li>').html(respuesta[especie].nombreCientifico).attr("id",respuesta[especie].nombreCientifico).hide().addClass('especie'));
 			$('#listaEspecies'+nombreCultivo).append($('<li/>').html("Nombre vulgar especie:").hover(function(){
 				$('#'+respuesta[especie].nombreCientifico).show();
 				$('#'+respuesta[especie].nombreCientifico+'tag').show();
-			}));
-			$('#listaEspecies'+nombreCultivo).append($('<li>').html(respuesta[especie].nombreVulgar).attr("id",respuesta[especie].nombreVulgar));
+			}).addClass('especie'));
+			$('#listaEspecies'+nombreCultivo).append($('<li>').html(respuesta[especie].nombreVulgar).attr("id",respuesta[especie].nombreVulgar).addClass('especie'));
 			$('#listaEspecies'+nombreCultivo).append($('<button onClick="funcionPlaga(\''+respuesta[especie].nombreVulgar+'\')">').html("Plagas").attr("id",respuesta[especie].nombreVulgar+'boton'));
 			$('#listaEspecies'+nombreCultivo).append($('<button onClick="funcionPlagaBotones(\''+respuesta[especie].nombreVulgar+'\')">').html("Ocultar").attr("id",respuesta[especie].nombreVulgar+'boton2').hide());
 		}
 		
 		});
 	}
-	
-	function funcionNomCientifico(nom){
-		alert(nom);
-	}
+
 	
 	function funcionEspecieBotones(nombreCultivo){
 		$('#'+nombreCultivo+'boton').show();
@@ -55,17 +52,17 @@ $(function() {
 		$('#'+nombreEspecie+'boton').hide();
 		$('#'+nombreEspecie+'boton2').show();
 		lista3 = $('#'+nombreEspecie);
-		lista3.append($('<ul/>').html("Plagas:").attr("id","listaPlagas"+nombreEspecie));
+		lista3.append($('<ul/>').html("Plagas:").attr("id","listaPlagas"+nombreEspecie).addClass('listaPlagas'));
 		for(plaga in respuesta){
-			$('#listaPlagas'+nombreEspecie).append($('<li/>').html("Nombre cientifico plaga:").attr("id",respuesta[plaga].nombreCientifico+'tag').hide());
-			$('#listaPlagas'+nombreEspecie).append($('<li>').html(respuesta[plaga].nombreCientifico).attr("id",respuesta[plaga].nombreCientifico).hide());
-			$('#listaPlagas'+nombreEspecie).append($('<li/>').html("URL plaga:"));
-			$('#listaPlagas'+nombreEspecie).append($('<li>').html(respuesta[plaga].url).attr("id",respuesta[plaga].url));
+			$('#listaPlagas'+nombreEspecie).append($('<li/>').html("Nombre cientifico plaga:").attr("id",respuesta[plaga].nombreCientifico+'tag').hide().addClass('plaga'));
+			$('#listaPlagas'+nombreEspecie).append($('<li>').html(respuesta[plaga].nombreCientifico).attr("id",respuesta[plaga].nombreCientifico).hide().addClass('plaga'));
+			$('#listaPlagas'+nombreEspecie).append($('<li/>').html("URL plaga:").addClass('plaga'));
+			$('#listaPlagas'+nombreEspecie).append($('<li>').html(respuesta[plaga].url).attr("id",respuesta[plaga].url).addClass('plaga'));
 			$('#listaPlagas'+nombreEspecie).append($('<li/>').html("Nombre vulgar plaga:").hover(function(){
 				$('#'+respuesta[plaga].nombreCientifico).show();
 				$('#'+respuesta[plaga].nombreCientifico+'tag').show();
-			}));
-			$('#listaPlagas'+nombreEspecie).append($('<li>').html(respuesta[plaga].nombreVulgar).attr("id",respuesta[plaga].nombreVulgar));
+			}).addClass('plaga'));
+			$('#listaPlagas'+nombreEspecie).append($('<li>').html(respuesta[plaga].nombreVulgar).attr("id",respuesta[plaga].nombreVulgar).addClass('plaga'));
 			$('#listaPlagas'+nombreEspecie).append($('<button onClick="funcionSustancia(\''+respuesta[plaga].nombreVulgar+'\')">').html("Sustancias").attr("id",respuesta[plaga].nombreVulgar+'boton'));
 			$('#listaPlagas'+nombreEspecie).append($('<button onClick="funcionSustanciaBotones(\''+respuesta[plaga].nombreVulgar+'\')">').html("Ocultar").attr("id",respuesta[plaga].nombreVulgar+'boton2').hide());
 		}
@@ -86,10 +83,10 @@ $(function() {
 		$('#'+nombrePlaga+'boton').hide();
 		$('#'+nombrePlaga+'boton2').show();
 		lista4 = $('#'+nombrePlaga);
-		lista4.append($('<ul/>').html("Sustancias:").attr("id","listaSustancias"+nombrePlaga));
+		lista4.append($('<ul/>').html("Sustancias:").attr("id","listaSustancias"+nombrePlaga).addClass('listaSustancias'));
 		for(sustancia in respuesta){
-			$('#listaSustancias'+nombrePlaga).append($('<li/>').html("Nombre sustancia:"));
-			$('#listaSustancias'+nombrePlaga).append($('<li>').html(respuesta[sustancia].nombre).attr("id",respuesta[sustancia].nombre));
+			$('#listaSustancias'+nombrePlaga).append($('<li/>').html("Nombre sustancia:").addClass('sustancia'));
+			$('#listaSustancias'+nombrePlaga).append($('<li>').html(respuesta[sustancia].nombre).attr("id",respuesta[sustancia].nombre).addClass('sustancia'));
 			$('#listaSustancias'+nombrePlaga).append($('<button onClick="funcionProducto(\''+respuesta[sustancia].nombre+'\')">').html("Productos").attr("id",respuesta[sustancia].nombre+'boton'));
 			$('#listaSustancias'+nombrePlaga).append($('<button onClick="funcionProductoBotones(\''+respuesta[sustancia].nombre+'\')">').html("Ocultar").attr("id",respuesta[sustancia].nombre+'boton2').hide());
 		}
@@ -110,12 +107,12 @@ $(function() {
 		$('#'+nombreSustancia+'boton').hide();
 		$('#'+nombreSustancia+'boton2').show();
 		lista5 = $('#'+nombreSustancia);
-		lista5.append($('<ul/>').html("Productos:").attr("id","listaProductos"+nombreSustancia));
+		lista5.append($('<ul/>').html("Productos:").attr("id","listaProductos"+nombreSustancia).addClass('listaProductos'));
 		for(producto in respuesta){
-			$('#listaProductos'+nombreSustancia).append($('<li/>').html("Nombre producto:"));
-			$('#listaProductos'+nombreSustancia).append($('<li>').html(respuesta[producto].nombre).attr("id",respuesta[producto].nombre));
-			$('#listaProductos'+nombreSustancia).append($('<li/>').html("URL producto:"));
-			$('#listaProductos'+nombreSustancia).append($('<li>').html(respuesta[producto].url).attr("id",respuesta[producto].url));
+			$('#listaProductos'+nombreSustancia).append($('<li/>').html("Nombre producto:").addClass('producto'));
+			$('#listaProductos'+nombreSustancia).append($('<li>').html(respuesta[producto].nombre).attr("id",respuesta[producto].nombre).addClass('producto'));
+			$('#listaProductos'+nombreSustancia).append($('<li/>').html("URL producto:").addClass('producto'));
+			$('#listaProductos'+nombreSustancia).append($('<li>').html(respuesta[producto].url).attr("id",respuesta[producto].url).addClass('producto'));
 		}
 		
 		});
